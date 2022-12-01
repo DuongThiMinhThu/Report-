6ad1eac23056147a9dd69b9bb55b4c0c418d7da2
Dương Thị Minh Thư <115393636+DuongThiMinhThu@users.noreply.github.com>
2022-12-02 01:42:34 +0700

    Update Báo cáo Customer 360.sql

--- Báo cáo Customer 360.sql
+++ Báo cáo Customer 360.sql
@@ -32,9 +32,10 @@ NTILE(4) over (order by frequency desc) as F,
 NTILE(4) over (order by monetary desc) as M from dbo.CRM_RFM_SUMMARY
 order by R desc 
 
-select concat(R,F,M) as RFM , count(*) as total_customers from (				
+select * into #temp_ex from (				
+select concat(R,F,M) as RFM from (				
 select CustomerID, recency ,frequency , monetary , case when recency >= 9 and recency < 40 then '4'				
-				     when recency >= 40 and recency < 68 then '3'
+				      when recency >= 40 and recency < 68 then '3'
 				     when recency >= 68 and recency < 80 then '2'
 				     else '1' end as R , 
 				     case when frequency >= 1 and frequency < 2 then '1'
@@ -45,8 +46,8 @@ select CustomerID, recency ,frequency , monetary , case when recency >= 9 and re
 				     when monetary >= 50000 and monetary < 100000 then '2'
 				     when monetary >= 100000 and monetary < 1000000 then '3'
 				     else '4' end as M
-				     from dbo.CRM_RFM_SUMMARY crs  ) A 
-group by concat(R,F,M)
-ORDER by total_customers desc
+				     from dbo.CRM_RFM_SUMMARY crs  ) A  )B
+		
+select RFM,count(*) as total_customers from #temp_ex group by RFM
 
 select min(frequency),max(frequency),avg(frequency) from dbo.CRM_RFM_SUMMARY crs 
